cc9f792eb4f204fe9ddc3f1d778b6b0d8684ab2b
yaster <fr87s.9875@gmail.com>
2016-10-28 11:10:10 +0900

    add edukit package

---
+++ 1st unity/Assets/edukit/endScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;//UIを使用するのに必要
+using UnityEngine.SceneManagement;//シーン遷移をするのに必要
+
+public class endScript : MonoBehaviour {
+
+	int _mytime;
+
+	[SerializeField, HeaderAttribute ("リザルト表示用テキストUI")]
+	public Text result;
+	[SerializeField, HeaderAttribute ("オープニングシーン名")]
+	public string opSceneName;
+
+	// Use this for initialization
+	void Start () {
+		_mytime = PlayerPrefs.GetInt ("mytime");
+		result.text = "リザルト：" + _mytime.ToString () + "ポイント";
+		Cursor.visible = true;
+		Cursor.lockState = CursorLockMode.None;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void goOp(){
+		SceneManager.LoadScene (opSceneName);
+	}
+}
